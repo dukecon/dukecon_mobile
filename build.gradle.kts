f529026429d866b2ec258de8b9b030465e6cd95a
Michal Harakal <michal.harakal@googlemail.com>
2019-12-22 21:06:49 +0100

    Add kotlin based gradle plugin for generating of project modules dependency graph. Based on idea of JakeWharton

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,15 @@ buildscript {
     repositories {
         google()
         jcenter()
-        maven { setUrl( "https://maven.fabric.io/public") }
-
-
+        maven { setUrl("https://maven.fabric.io/public") }
     }
     dependencies {
-        classpath (BuildPlugins.androidGradlePlugin)
-        classpath (BuildPlugins.kotlinGradlePlugin)
-        classpath (BuildPlugins.kotlinSerializationGradlePlugin)
-        classpath ("com.google.gms:google-services:4.3.2")
-        classpath ("io.spring.gradle:dependency-management-plugin:1.0.7.RELEASE")
-        classpath ("io.fabric.tools:gradle:1.29.0")
-
+        classpath(BuildPlugins.androidGradlePlugin)
+        classpath(BuildPlugins.kotlinGradlePlugin)
+        classpath(BuildPlugins.kotlinSerializationGradlePlugin)
+        classpath("com.google.gms:google-services:4.3.3")
+        classpath("io.spring.gradle:dependency-management-plugin:1.0.7.RELEASE")
+        classpath("io.fabric.tools:gradle:1.31.0")
     }
 }
 
@@ -26,4 +23,8 @@ allprojects {
 
 tasks.register("clean").configure {
     delete("build")
+}
+
+plugins {
+    id("project-dependencies-graph-plugin")
 }
