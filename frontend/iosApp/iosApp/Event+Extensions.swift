41bc2e8a16ad3b76ee2107a1610231cbd1130118
Michal Harakal <Alex@vonBelow.Com>
2020-02-22 19:15:07 +0100

    Added utility functions to multi-platform types

---
+++ frontend/iosApp/iosApp/Event+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Event+Extensions.swift
+//  iosApp
+//
+//  Created by Alexander v. Below on 22.02.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import MultiPlatformLibrary
+
+// These look like they might be good candiates for the common
+// Kotlin library. These are tasks required on both platforms
+
+extension Event_ {
+    var speakerList : String {
+        let stringmap = self.speakers.map { (speaker) -> String in
+            return speaker.name
+        }
+        return stringmap.joined(separator: ", ")
+    }
+}
+
+extension Location_ {
+    var localizedName : String {
+        if let languageCode = Locale.autoupdatingCurrent.languageCode, let localizedName = self.names[languageCode] {
+            return localizedName
+        } else if let key = names.keys.first, let name = names[key] {
+            return name
+        } else {
+            return ""
+        }
+    }
+}
