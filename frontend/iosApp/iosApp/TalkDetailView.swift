f328f9bca679b0adffc8166c8c60a71b45ea390f
Michal Harakal <Alex@vonBelow.Com>
2020-02-26 10:41:46 +0100

    [iOS] First simple implementation of Speaker Images

--- frontend/iosApp/iosApp/TalkDetailView.swift
+++ frontend/iosApp/iosApp/TalkDetailView.swift
@@ -14,6 +14,24 @@ struct SpeakerViewModel {
     var imageURL: URL? = nil
 }
 
+class ImageProvider: ObservableObject {
+    @Published var image: UIImage = UIImage(named: "sample")!
+    var url: URL? {
+        didSet {
+            if let url = url {
+                URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+                    if let data = data, error == nil {
+                        guard let image = UIImage(data: data) else {
+                            return
+                        }
+                        self.image = image
+                    }
+                }.resume()
+            }
+        }
+    }
+}
+
 struct TalkDetailViewModel {
     var title: String
     var room: String
@@ -26,6 +44,19 @@ struct TalkDetailView: View {
     var room: String
     var description: String
     var speakers: [SpeakerViewModel]
+
+    @ObservedObject var imageProvider = ImageProvider()
+
+    init(viewModel: TalkDetailViewModel) {
+        self.title = viewModel.title
+        self.room = viewModel.room
+        self.description = viewModel.description
+        self.speakers = viewModel.speakers
+
+        if let speaker = speakers.first, let imageUrl = speaker.imageURL {
+            imageProvider.url = imageUrl
+        }
+    }
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -46,10 +77,12 @@ struct TalkDetailView: View {
                 HStack {
                     ForEach(speakers, id:\.name ) {
                         speaker in
-                        // speakerImage.resizable().frame(width: 40, height: 40, alignment: .center).clipShape(Circle())
-                        VStack {
-                            Text (speaker.name)
-                            Text (speaker.subtitle)
+                        HStack {
+                            Image(uiImage: self.imageProvider.image).resizable().frame(width: 40, height: 40, alignment: .center).clipShape(Circle())
+                            VStack {
+                                Text (speaker.name)
+                                Text (speaker.subtitle)
+                            }
                         }
                     }
                     Spacer()
@@ -63,6 +96,7 @@ struct TalkDetailView: View {
 
 struct TalkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TalkDetailView(title: "Kotlin", room: "Shanghai", description: "Lorem Ipsum", speakers:[SpeakerViewModel(name: "Alexander von Below", subtitle: "Deutsche Telekom AG")])
+        let viewModel = TalkDetailViewModel(title: "Kotlin", room: "Foo", description: "Lorem Ipsum", speakers: [SpeakerViewModel(name: "Alexander von Below", subtitle: "Deutsche Telekom AG")])
+        return TalkDetailView(viewModel: viewModel)
     }
 }
