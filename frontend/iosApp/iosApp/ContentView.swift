fe61c0ccb1076048db3fd1f6822124714eaaf47e
Alexander von Below <Alex@vonBelow.Com>
2020-02-29 16:05:44 +0100

    [iOS] Outfactored the SpeakerView

--- frontend/iosApp/iosApp/ContentView.swift
+++ frontend/iosApp/iosApp/ContentView.swift
@@ -16,25 +16,7 @@ struct ContentView: View {
     var body: some View {
         TabView(selection: $selection){
             NavigationView {
-                VStack {
-                    HStack {
-                        ForEach(eventPublisher.dates, id:\.timestamp ) { day in
-                            Button(action: {
-                                self.eventPublisher.day = day.dayOfMonth
-                            }) {
-                                Text(day.dayOfWeek.value)
-                            }
-                        }
-                    }
-                    
-                    List(eventPublisher.events, id: \.eventId) { event in
-                        VStack (alignment: .leading) {
-                            NavigationLink(destination: TalkDetailView(viewModel: event.talkDetailViewModel)) {
-                                TalkView(title: event.title, speakers: event.speakerList, room: event.room.localizedName)
-                            }
-                        }
-                    }.font(.title)
-                }
+                SpeakerView()
             }.navigationBarTitle(Text("action_schedule"))
                 .tabItem {
                     VStack {
---
+++ frontend/iosApp/iosApp/SpeakerView.swift
@@ -0,0 +1,40 @@
+//
+//  SpeakerView.swift
+//  iosApp
+//
+//  Created by Alexander v. Below on 29.02.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpeakerView: View {
+    @EnvironmentObject var eventPublisher: EventsPublisher
+
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(eventPublisher.dates, id:\.timestamp ) { day in
+                    Button(action: {
+                        self.eventPublisher.day = day.dayOfMonth
+                    }) {
+                        Text(day.dayOfWeek.value)
+                    }
+                }
+            }
+
+            List(eventPublisher.events, id: \.eventId) { event in
+                VStack (alignment: .leading) {
+                    NavigationLink(destination: TalkDetailView(viewModel: event.talkDetailViewModel)) {
+                        TalkView(title: event.title, speakers: event.speakerList, room: event.room.localizedName)
+                    }
+                }
+            }.font(.title)
+        }    }
+}
+
+struct SpeakerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeakerView()
+    }
+}
